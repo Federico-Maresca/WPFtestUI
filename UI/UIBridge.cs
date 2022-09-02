71bc7a84c797b862e15678bb222ff1815157c693
fmare <fmare@FEDERICO>
2022-09-02 13:41:32 +0200

    Add project files.

---
+++ UI/UIBridge.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UI.View;
+using UI;
+using System.Threading; //// Add this in your file
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+using System.Windows.Media.Media3D;
+
+class UIBridge
+{
+
+    public static MainView mainview_ui;
+    public static Thread gui_thread;
+    public static IntPtr mainview_handle = IntPtr.Zero;
+
+
+    [DllExport]
+    static public IntPtr CreateUserInterface
+           (uint parentthread) /// Multi-Threaded Version
+    {
+
+        gui_thread = new Thread(() =>
+        {
+            mainview_ui = new MainView(parentthread)
+            { Opacity = 0, Width = 1280, Height = 720 };
+            mainview_ui.Show();
+            mainview_handle = new WindowInteropHelper(mainview_ui).Handle;
+            System.Windows.Threading.Dispatcher.Run();
+
+            mainview_ui.Hide();
+            mainview_ui.Opacity = 1;
+        });
+        gui_thread.SetApartmentState(ApartmentState.STA); /// STA Thread Initialization
+        gui_thread.Start();
+
+        while (mainview_handle == IntPtr.Zero) { }
+        return mainview_handle;
+    }
+
+    //[DllExport]
+    //static public int GetGuiThread()
+    //{
+    //    return gui_thread.ManagedThreadId; 
+    //}
+    [DllExport]
+    static public void DisplayUserInterface() /// Multi-Threaded Version
+    {
+        try
+        {
+            mainview_ui.Show();
+            //mainview_ui.Opacity = 1;
+            //mainview_ui.Width = 1280 ;
+            //mainview_ui.Height = 720;
+    }
+        catch /// Can't Access to UI Thread, So Dispatching
+        {
+            mainview_ui.Dispatcher.BeginInvoke((Action)(() => {
+                mainview_ui.Show();
+                mainview_ui.Opacity = 1;
+            }));
+        }
+    }
+
+    [DllExport]
+    static public void DestroyUserInterface() /// Multi-Threaded Version
+    {
+        try
+        {
+            mainview_ui.Close();
+        }
+        catch /// Can't Access to UI Thread, So Dispatching
+        {
+            mainview_ui.Dispatcher.BeginInvoke((Action)(() => {
+                mainview_ui.Close();
+            }));
+        }
+    }
+    [DllExport]
+    static public void HideUserInterface() /// Multi-Threaded Version
+    {
+        try
+        {
+            mainview_ui.Hide();
+        }
+        catch /// Can't Access to UI Thread, So Dispatching
+        {
+            mainview_ui.Dispatcher.BeginInvoke((Action)(() => {
+                mainview_ui.Hide();
+            }));
+        }
+    }
+
+}
---
+++ UI/View/MainView.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using UI;
+namespace UI.View
+{
+
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        [DllImport("User32.dll", SetLastError = true, EntryPoint = "FindWindow")]
+        public static extern IntPtr FindWindow(String lpClassName, String lpWindowName);
+
+        [DllImport("User32.dll", SetLastError = true, EntryPoint = "SendMessage")]
+        static extern long SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+        [DllImport("User32.dll", SetLastError = true, EntryPoint = "SendMessage")]
+        public static extern IntPtr SendMessageSIMPLE(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        [return: MarshalAs(UnmanagedType.Bool)]
+        [DllImport("user32.dll", SetLastError = true, EntryPoint = "PostThreadMessageA")]
+        public static extern bool PostThreadMessage(uint threadId, uint msg, UIntPtr wParam, IntPtr lParam);
+
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto, Pack = 1)]
+        public struct COPYDATASTRUCT
+        {
+            public uint dwData;    // Any value the sender chooses.  Perhaps its main window handle?
+            public int cbData;    // The count of bytes in the message.
+            public IntPtr lpData;    // The address of the message.
+        }
+
+        public static IntPtr IntPtrAlloc<T>(T param)
+        {
+            IntPtr retval = Marshal.AllocHGlobal(Marshal.SizeOf(param));
+            Marshal.StructureToPtr(param, retval, false);
+            return (retval);
+        }
+
+        public static void IntPtrFree(IntPtr preAllocated)
+        {
+            if (IntPtr.Zero == preAllocated) throw (new Exception("Go Home"));
+            Marshal.FreeHGlobal(preAllocated); preAllocated = IntPtr.Zero;
+        }
+
+        const uint WM_PRINT = 1025;
+        const uint WM_SEND = 1026;
+        public uint threadId;
+        public uint threadIdMine;
+        delegate bool testprintptr(string filename);
+        delegate bool testsendptr(string filename, long filesize);
+        public IntPtr parentwindowfind;
+        public IntPtr parentwindowmine;
+        public int amount;
+        public MainView(uint parentthread)
+        {
+            InitializeComponent();
+            //testprint = (testprintptr)Marshal.GetDelegateForFunctionPointer
+            //        (api_1_ptr, typeof(testprintptr));
+            //testsend = (testsendptr)Marshal.GetDelegateForFunctionPointer
+            //                      (api_2_ptr, typeof(testsendptr));
+            amount = 1000;
+            //MyFindWindow();
+            threadId = parentthread;
+        }
+
+
+
+        public void MyFindWindow()
+        {
+
+            IntPtr hwnd = FindWindow("CppMAppHostWinClass", "TEST");
+            while (hwnd == IntPtr.Zero)
+            {
+                Thread.Sleep(5);
+                hwnd = FindWindow("CppMAppHostWinClass", "TEST");
+            }
+            this.parentwindowfind = hwnd;
+        }
+
+        private void Print_Click(object sender, RoutedEventArgs e)
+        {
+            PostThreadMessage(threadId, WM_PRINT, UIntPtr.Zero, IntPtr.Zero);
+           // SendMessage(parentwindowfind, WM_PRINT, IntPtr.Zero, IntPtr.Zero);
+
+        }
+
+        private void Send_Click(object sender, RoutedEventArgs e)
+        {
+
+            IntPtr send = new IntPtr(amount);
+            PostThreadMessage(threadId, WM_SEND, UIntPtr.Zero, send);
+            //SendMessage(parentwindowfind, WM_SEND, IntPtr.Zero, send);
+        }
+    }
+}
